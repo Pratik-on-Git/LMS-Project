6feea771687eefd74636ed47914a48fd88f0db89
Pratik Das <pratikdasmartingarrix@gmail.com>
2026-01-21 12:20:46 +0530

    feat: add @t3-oss/env-nextjs package and create environment configuration

---
+++ lms-project/lib/env.ts
@@ -0,0 +1,11 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+ 
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.url(),
+    OPEN_AI_API_KEY: z.string().min(1),
+  },
+  // For Next.js >= 13.4.4, you only need to destructure client variables:
+  experimental__runtimeEnv: {},
+});
