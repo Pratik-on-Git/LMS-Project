e6c80801f08a938d62cf1c745486a0f82a5f8df1
Pratik Das <pratikdasmartingarrix@gmail.com>
2026-01-23 10:21:57 +0530

    feat(admin): refactor AdminIndexPage and add CoursesPage layout

---
+++ lms-project/app/admin/courses/page.tsx
@@ -0,0 +1,19 @@
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function CoursesPage() {
+    return (
+        <>
+            <div className="flex items-center justify-between">
+                <h1 className="text-2xl font-semibold">Your Courses</h1>
+
+                <Link href="/admin/courses/new" className={buttonVariants()}>
+                    Add New Course
+                </Link>
+            </div>
+            <div>
+                <h1>Courses List</h1>
+            </div>
+        </>
+    );
+}
